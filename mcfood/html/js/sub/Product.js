524a8da1aeaff0a69908126a5ee91cc300bb29c9
giseok <hgsok3056@naver.com>
2021-05-05 22:53:32 +0900

    05/05 sub.css 조건별 배송 span 수정

--- mcfood/html/js/sub/Product.js
+++ mcfood/html/js/sub/Product.js
@@ -1,4 +1,5 @@
 // sub 페이지에서 이미지 클릭 시 교체되는 함수
+// window.onload 이부분이 없으면 실행되지 않음.
 window.onload = function () {
   var bigPic = document.querySelector("#bigimg"); 
   var smallPics = document.querySelectorAll(".smallimg"); 

e1508e2a681827708030747cb94cfc10386d8585
giseok <hgsok3056@naver.com>
2021-06-01 18:44:00 +0900

    06/01 저녁 진행 상황 : main_max_599.css 작성 완료

--- exhibiz/html/js/Header.js
+++ exhibiz/html/js/Header.js
@@ -1,17 +1,15 @@
-window.onload = function () {
-  $(document).ready(function(){
-    $(".menuicon").click(function(){
-      $(".gnb").slideToggle();
-    });
-    $(window).resize(function(){
-      if(window.innerWidth >= 1100) {
-        $(".gnb").show();
-      }
-    });
-    $(window).resize(function(){
-      if(window.innerWidth <= 1099) {
-        $(".gnb").hide();
-      }
-    });
+$(document).ready(function () {
+  $(".menuicon").click(function () {
+    $(".gnb").slideToggle();
   });
-}
+  $(window).resize(function () {
+    if (window.innerWidth >= 1100) {
+      $(".gnb").show();
+    }
+  });
+  $(window).resize(function () {
+    if (window.innerWidth <= 1099) {
+      $(".gnb").hide();
+    }
+  });
+});

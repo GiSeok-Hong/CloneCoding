406d4d993dcb1b76df1fedde41344725e10f0acd
giseok <hgsok3056@naver.com>
2021-07-30 12:28:02 +0900

    07/30 오전 작업 현황 - header 작성 중

---
+++ kyobobook/html/js/header.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+  
+  // 최상단 배너
+  $(".header_banner .close").click(function(){
+    $(".header_banner").stop().slideUp();
+  });
+
+
+  // 바로콘 
+  $(".barocon").mouseover(function(){
+    $(".barocon_none").stop().show();
+  }).mouseout(function(){
+    $(".barocon_none").stop().hide();
+  });
+
+
+  // 회원혜택
+  $(".benefits").mouseover(function(){
+    $(this).children(".benefits_none").stop().show();
+  }).mouseout(function(){
+    $(".benefits_none").stop().hide();
+  });
+
+
+  // 인기검색어
+
+
+
+  // 헤더 광고
+  $(".search_ad > .prev").click(function(){
+    $(".search_ad > ul > li:last").prependTo(".search_ad > ul");
+    $(".search_ad > ul").css({marginLeft:-220});
+    $(".search_ad > ul").stop().animate({ marginLeft: 0 });
+    
+  });
+  
+  $(".search_ad > .next").click(function(){
+    $(".search_ad > ul").stop().animate({marginLeft:-220},function(){
+      $(".search_ad > ul > li:first").appendTo(".search_ad > ul");
+      $(".search_ad > ul").css({marginLeft:0});
+    });
+  });
+
+
+  // 네비게이션 바
+  $(".nav_main > li").mouseover(function(){
+    $(this).children("ul").stop().show();
+  }).mouseout(function(){
+    $(this).children("ul").stop().hide();
+  });
+
+});

494b71079d71435f11cb542102e219b9a04bd0ac
giseok <hgsok3056@naver.com>
2021-08-02 14:08:51 +0900

    08/02 only_kbook 구간 작성

---
+++ kyobobook/html/js/only_kbook.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $.ajax({
+    url: "json/only_kbook.json",
+    dataType: "json",
+    success : function(data){
+      if(data.length > 0){
+        for(var i in data){
+          var url = data[i].url;
+
+          $(".only_kbook ul").append( '<li><a href="#" target="_self">'+"<img src='img/"+data[i].url+ "' " + 'alt="icon'+ (i+1) +'"></a></li>');
+
+        }
+      }
+    }
+  });
+
+
+  $('.only_kbook .prev').click(function(){
+		$('.only_kbook li').slice(7).prependTo('.only_kbook ul');
+		$('.only_kbook li').hide().filter(':lt(7)').show();
+	});
+	$('.only_kbook .next').click(function(){
+		$('.only_kbook li').slice(0,7).appendTo('.only_kbook ul');
+		$('.only_kbook li').hide().filter(':lt(7)').show()
+	});
+
+});

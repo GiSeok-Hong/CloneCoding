dfd23a52e9229903734d8fe4169dcbfb7e092903
giseok <hgsok3056@naver.com>
2021-08-03 17:15:34 +0900

    08/03 slider 구간 작성

--- kyobobook/html/js/slider.js
+++ kyobobook/html/js/slider.js
@@ -15,30 +15,74 @@ $(document).ready(function () {
   });
 
 
+  var bg_color=["rgb(230, 233, 246)", "rgb(168, 218, 255)", "rgb(226, 242, 211)", "rgb(218, 236, 240)", "rgb(248, 244, 216)", "rgb(222, 243, 228)", "rgb(251, 234, 239)"];
+  var i = 0;
+  
+  // 슬라이드 이전
+  function prev() {
+    $(".slide_show > li").eq(i).stop().hide();
+    i--;
+    if(i < 0 ){
+      i = $(".slide_show > li:last").index();
+    }
+    $(".slide_show > li").eq(i).stop().show();
+    $('#slider').css("background-color",bg_color[i]);
+    $('.slide_nav > li').removeClass('current');
+		$('.slide_nav > li').eq(i).addClass('current');
+  }
 
-  var i = 0;    
+  // 슬라이드 다음
   function next(){
     i++;
     if(i > $(".slide_show > li:last").index()){
       i = 0;
     }
     $(".slide_show > li").eq(i).stop().show();
-    $(".slide_show > li").eq(i-1).stop().hide();
+    $('#slider').css("background-color",bg_color[i]);
+    $(".slide_show > li").eq(i - 1).stop().hide();
+    $('.slide_nav > li').removeClass('current');
+		$('.slide_nav > li').eq(i).addClass('current');
   }
-  
-  setInterval(next,2000);
 
+
+  // 슬라이드 버튼 show / hide
+  $(".slider_left").mouseover(function () {
+    $(".slider_left > div").show();
+  }).mouseout(function () {
+    $(".slider_left > div").hide();
+  });
+
+
+  $('.slider_left > .prev').click(function(){
+    prev();
+	});
+  $('.slider_left > .next').click(function(){
+    next();
+	});
+
+  // 슬라이드 3초마다 다음 페이지
+  setInterval(next,3000);
+
+
+  // 슬라이드 네비게이션
+  $('.slide_nav > li').mouseover(function () {
+    i = $(this).index();
+		$('#slider').css("background-color",bg_color[i]);
+		$('.slide_show > li').stop().hide();
+    $('.slide_show > li').eq(i).stop().show();
+    $('.slide_nav > li').removeClass('current');
+		$(this).addClass('current');
+	});
+
+  
+  // book_news
+  $(".book_news > .close").click(function(){
+    $(".book_news").stop().hide();
+  });
+
+  // slider_ad
+  $(".slider_ad > .close").click(function(){
+    $(".slider_ad").stop().hide();
+  });
 
 });  /* end */
-
-
-
-
-
-
-// $('.slide_nav > li').mouseover(function () {
-//   var finder = $(this).attr('class');
-//   $('.slide_show > li').hasClass(finder).show();
-  
-  
-// });

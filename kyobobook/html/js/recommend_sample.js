a207b19187c5ba12f75d3bc65f3bd028422b4635
giseok <hgsok3056@naver.com>
2021-08-04 11:39:03 +0900

    08/04 recommend_sample 작성

---
+++ kyobobook/html/js/recommend_sample.js
@@ -0,0 +1,31 @@
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=title",
+  data:{ query:"추천" },
+  headers:{Authorization: "KakaoAK bc5789bb714b0042c7805de6ae8b6934"}
+})
+.done(function( msg ) {
+
+  var box = $(".recommend_sample > .itembox > .item > .sample")
+
+  for(var i=0; i < box.length; i++){
+    var str=msg.documents[i].contents;
+    var str2 = str.substring(0, 80);
+
+    // 이미지
+    box.eq(i).append('<a href="#">' + '<img src="' + msg.documents[i].thumbnail + '"/><div>' + str2 +'</div></a>');
+    
+    // 책 제목
+    box.eq(i).append('<h5><a href="#">' + msg.documents[i].title + '</a></h5>');
+    
+    // 작가
+    box.eq(i).append('<h6>' + msg.documents[i].authors + '</h6>');
+    
+    // 정가, 판매가
+    box.eq(i).append('<span>정가 : '+msg.documents[i].price+'</span><span>판매가 : '+msg.documents[i].sale_price+'</span>');
+
+
+
+  }
+
+});

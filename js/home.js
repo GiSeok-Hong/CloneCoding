077da13919c7bd47fa04fbfeafce1a409b52ad19
giseok <hgsok3056@naver.com>
2021-09-02 21:15:26 +0900

    09/02 index.html - section1 이미지 및 배경색 변경

--- js/home.js
+++ js/home.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-  function braket_move() {
-    $("#braket_img").stop().animate({ marginTop: "40px" }, 700, function () {
-      $("#braket_img").stop().animate({ marginTop: "0" }, 700);
+  function ajax_move() {
+    $("#ajax_img").stop().animate({ marginTop: "40px" }, 700, function () {
+      $("#ajax_img").stop().animate({ marginTop: "0" }, 700);
     });
   }
-  setInterval(braket_move, 1400);
+  setInterval(ajax_move, 1400);
   
   function x_move() {
     $("#x_img").stop().animate({ marginBottom: "15px" }, 800, function () {
@@ -13,33 +13,33 @@ $(document).ready(function(){
   }
   setInterval(x_move, 1600);
 
-  function jquery_move(){
-    $("#jquery_img").stop().animate({ marginBottom: "40px" }, 1000, function () {
-      $("#jquery_img").stop().animate({ marginBottom: "0" }, 1000);
+  function js_move(){
+    $("#js_img").stop().animate({ marginBottom: "40px" }, 1000, function () {
+      $("#js_img").stop().animate({ marginBottom: "0" }, 1000);
     });
   }
-  setInterval(jquery_move, 2000);
+  setInterval(js_move, 2000);
 
-  function html_move() {
-    $("#html_img").stop().animate({ marginBottom: "25px" }, 500, function () {
-      $("#html_img").stop().animate({ marginBottom: "0" }, 500);
+  function api_move() {
+    $("#api_img").stop().animate({ marginBottom: "25px" }, 500, function () {
+      $("#api_img").stop().animate({ marginBottom: "0" }, 500);
     });
   }
-  setInterval(html_move, 1000);
+  setInterval(api_move, 1000);
 
-  function css_move() {
-    $("#css_img").stop().animate({ marginTop: "40px" }, 500, function () {
-      $("#css_img").stop().animate({ marginTop: "0" }, 500);
+  function json_move() {
+    $("#json_img").stop().animate({ marginTop: "40px" }, 500, function () {
+      $("#json_img").stop().animate({ marginTop: "0" }, 500);
     });
   }
-  setInterval(css_move, 1000);
+  setInterval(json_move, 1000);
 
-  function js_move() {
-    $("#js_img").stop().animate({ marginBottom: "50px" }, 500, function () {
-      $("#js_img").stop().animate({ marginBottom: "0" }, 500);
+  function media_move() {
+    $("#media_img").stop().animate({ marginBottom: "50px" }, 500, function () {
+      $("#media_img").stop().animate({ marginBottom: "0" }, 500);
     });
   }
-  setInterval(js_move, 1000);
+  setInterval(media_move, 1000);
 
   function setting_move() {
     $("#setting_img").stop().animate({ marginTop: "35px" }, 1000, function () {
